04b3382dc9775be5ddbfd78413a5f422d93e62dc
vahidolam <38908549+vahidolam@users.noreply.github.com>
2018-05-04 17:26:16 +0530

    comment added in main

--- crud/src/main/java/com/olam/crud/CrudApplication.java
+++ crud/src/main/java/com/olam/crud/CrudApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CrudApplication {
 
+		// this is main app
 	public static void main(String[] args) {
 		SpringApplication.run(CrudApplication.class, args);
 	}
