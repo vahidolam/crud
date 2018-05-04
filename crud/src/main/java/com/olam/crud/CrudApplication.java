2c7c50d509eadc443d71091d93cb4231cb9c47cf
vahidolam <38908549+vahidolam@users.noreply.github.com>
2018-05-04 17:32:30 +0530

    uat committed

--- crud/src/main/java/com/olam/crud/CrudApplication.java
+++ crud/src/main/java/com/olam/crud/CrudApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CrudApplication {
 
 		// this is main app
+		// this is uat
 	public static void main(String[] args) {
 		SpringApplication.run(CrudApplication.class, args);
 	}
